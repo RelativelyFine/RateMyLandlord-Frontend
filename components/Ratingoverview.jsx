ec19458956254eb577e272f58eef4cd810f7f49f
David <53689532+RelativelyFine@users.noreply.github.com>
2022-08-20 08:05:00 -0400

    Basic Template for RatingOverview

---
+++ components/Ratingoverview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ratingoverview = () => {
+  return (
+    <div className="flex bg-[#E2E2E2] w-full h-full rounded-2xl">
+      <div className="flex-col"></div>
+    </div>
+  );
+};
+
+export default Ratingoverview;
