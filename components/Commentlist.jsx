2e460bdb63e1b9fbe69c48e821cb6d5fa8bffb06
David <53689532+RelativelyFine@users.noreply.github.com>
2022-08-20 08:04:35 -0400

    Basic template for CommentList

---
+++ components/Commentlist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Commentlist = () => {
+  return (
+    <div className="flex bg-[#3C3C3C] w-full h-[70vh]">
+      <div className="grid w-full grid-cols-12 md:grid-cols-12 gap-[5em] px-12 content-center"></div>
+    </div>
+  );
+};
+
+export default Commentlist;
